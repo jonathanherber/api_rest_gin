f252daf0c091c899cdfd713dfb2366eac705ebe6
Jonathan Herber <jonathanherber15@gmail.com>
2022-04-06 10:37:06 -0300

    gorm

--- models/students.go
+++ models/students.go
@@ -1,9 +1,24 @@
 package models
 
+import "gorm.io/gorm"
+
 type Student struct { //struct for Students
+	gorm.Model
 	Name string `json:"name"` //formating to json
 	CPF  string `json:"cpf"`
 	RG   string `json:"rg"`
 }
 
 var Students []Student //slice of Student
+
+/*
+//gorm.Model in the structure builds a db table with:
+type Student struct {
+ID unit `gorm:"primaryKey"`
+CreatedTime time.Time
+UpdatedTime time.Time
+DeletedAt time.Time
+DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+//its called a embedded struct
+*/
--- server.go
+++ server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/jonathanherber/golang_gin/database"
 	"github.com/jonathanherber/golang_gin/models"
 	"github.com/jonathanherber/golang_gin/routes"
 )
 
 func main() {
+	database.ConnectToDatabase() //func to connect to database and execute the AutoMigrate
 	models.Students = []models.Student{
 		{Name: "Jonathan", CPF: "123456789", RG: "123123123"},
 		{Name: "Daniel", CPF: "987654321", RG: "321321321"},

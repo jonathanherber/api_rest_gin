f1e8c9a12d07530604efe816dde2faa8648dea7a
Jonathan Herber <jonathanherber15@gmail.com>
2022-04-10 18:53:24 -0300

    tests

--- main_test.go
+++ main_test.go
@@ -8,13 +8,31 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jonathanherber/golang_gin/controllers"
+	"github.com/jonathanherber/golang_gin/database"
+	"github.com/jonathanherber/golang_gin/models"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func RouteTestSetup() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	routes := gin.Default()
 	return routes
 }
+func CreateStudent() {
+	student := models.Student{
+		Name: "Test Student",
+		CPF:  "12345678901",
+		RG:   "123456789"}
+	database.DB.Create(&student)
+	ID = int(student.ID)
+}
+func DeleteStudent() {
+	var student models.Student
+	database.DB.Delete(&student, ID)
+}
+
 func TestVerifyGreetings(t *testing.T) {
 	r := RouteTestSetup() //new instance of GIN
 	r.GET("/:name", controllers.Greetings)
@@ -25,4 +43,27 @@ func TestVerifyGreetings(t *testing.T) {
 	mockResponse := `{"API:":"Paramater value Jonathan"}`
 	responseBody, _ := ioutil.ReadAll(response.Body)
 	assert.Equal(t, mockResponse, string(responseBody))
+}
+func TestGetStudents(t *testing.T) {
+	database.ConnectToDatabase()
+	CreateStudent()
+	defer DeleteStudent()
+	r := RouteTestSetup()
+	r.GET("/students", controllers.ShowStudents)
+	request, _ := http.NewRequest("GET", "/students", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+	//fmt.Println(response.Body)
+}
+func TestGetStudentByCpf(t *testing.T) {
+	database.ConnectToDatabase()
+	CreateStudent()
+	defer DeleteStudent()
+	r := RouteTestSetup()
+	r.GET("/students/cpf/:cpf", controllers.GetStudentByCPF)
+	request, _ := http.NewRequest("GET", "/students/cpf/03359110012", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
 }

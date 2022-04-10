a9e256410bf838020cfdaa8fc9c4dd318c65c1d7
Jonathan Herber <jonathanherber15@gmail.com>
2022-04-10 20:14:33 -0300

    tests

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -66,4 +69,50 @@ func TestGetStudentByCpf(t *testing.T) {
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, request)
 	assert.Equal(t, http.StatusOK, response.Code)
+}
+func TestGetStudentById(t *testing.T) {
+	database.ConnectToDatabase()
+	CreateStudent()
+	defer DeleteStudent()
+	r := RouteTestSetup()
+	r.GET("/students/:id", controllers.GetStudentById)
+	searchPath := "/students/" + strconv.Itoa(ID)
+	request, _ := http.NewRequest("GET", searchPath, nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+	var studentMock models.Student
+	json.Unmarshal(response.Body.Bytes(), &studentMock)
+	assert.Equal(t, "Test Student", studentMock.Name, "names must be equal")
+	assert.Equal(t, "12345678901", studentMock.CPF, "cpf must be equal")
+	assert.Equal(t, "123456789", studentMock.RG, "rg must be equal")
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+func TestDeleteStudent(t *testing.T) {
+	database.ConnectToDatabase()
+	CreateStudent()
+	r := RouteTestSetup()
+	r.DELETE("/students/:id", controllers.DeleteStudent)
+	searchPath := "/students/" + strconv.Itoa(ID)
+	request, _ := http.NewRequest("DELETE", searchPath, nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+func TestEditStudent(t *testing.T) {
+	database.ConnectToDatabase()
+	CreateStudent()
+	defer DeleteStudent()
+	r := RouteTestSetup()
+	r.PATCH("/students/:id", controllers.EditStudentById)
+	student := models.Student{Name: "Test Student", CPF: "10987654321", RG: "123456700"}
+	jsonValue, _ := json.Marshal(student)
+	searchPath := "/students/" + strconv.Itoa(ID)
+	request, _ := http.NewRequest("PATCH", searchPath, bytes.NewBuffer(jsonValue))
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+	var studentMockUpdated models.Student
+	json.Unmarshal(response.Body.Bytes(), &studentMockUpdated)
+	assert.Equal(t, "10987654321", studentMockUpdated.CPF)
+	assert.Equal(t, "123456700", studentMockUpdated.RG)
+	assert.Equal(t, "Test Student", studentMockUpdated.Name)
 }
